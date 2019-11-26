b3858214dd2cefcd1557ebbff87e53cab93e09e2
Mark Leo <ratrateroo@gmail.com>
2019-11-27 01:59:47 +0800

    handled REST API CORS errors

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ const app = express();
 
 //app.use(bodyParser.urlencoded()); //x-www-form-urlencoded <form>
 app.use(bodyParser.json()); //application/json HEADER
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+ 
+
 app.use('/feed', feedRoutes);
 
 
